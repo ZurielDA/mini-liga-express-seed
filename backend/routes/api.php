8b61c6bfe09525c2ec57a3066e798df47239af64
ZURIEL DA <ZURIEL_DIAZ_AGUSTIN@hotmail.com>
2025-11-01 18:48:15 -0600

    Feat(Backend - SportMatchController): Implement index, save and result (update) method using SportMatchService, and add API routes.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/teams', [TeamController::class, 'index']);
 Route::post('/teams', [TeamController::class, 'save']);
 
+Route::get('/matches', [SportMatchController::class, 'index']);
+Route::post('/matches', [SportMatchController::class, 'save']);
 Route::post('/matches/{id}/result', [SportMatchController::class, 'result']);
 
 Route::get('/standings', [StandingsController::class, 'index']);
